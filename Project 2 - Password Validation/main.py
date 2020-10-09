dfc8f8754497f499f708364225c75f4c2518aaf1
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-10-09 08:37:13 +0200

    Completed Project 2 (Section 14)

---
+++ Project 2 - Password Validation/main.py
@@ -0,0 +1,28 @@
+"""
+This module implements the passworkd validation exercise of the Section 14 of the course.
+"""
+
+import re
+
+
+def validate_password():
+    # Password to validate
+    password = "Felix123"
+
+    # Compile the pattern:
+    # · At least 8 char long
+    # · Contain any sort letters, numbers or $%#@
+    # · Has to end with a number
+    pattern = re.compile("([A-Za-z0-9$%#@]{7,}[0-9])")
+
+    # Check the password
+    check = pattern.fullmatch(password)
+
+    # Print the results
+    print(f"Validated => {check is not None}")
+
+
+# Entry point of the program
+if __name__ == '__main__':
+    # Call the validate password function
+    validate_password()
